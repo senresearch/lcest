51e8be75c6573b19dd37dba42ba1ac664950de20
Emily Hanson <hanea-17@rhodes.edu>
2016-08-08 19:11:57 -0500

    Added functions and documentation for likBivT and assessory functions and blcest. Also edited documentation for likBivNorm and assessories. Note documentation for blcest is not complete

---
+++ R/censor_function.R
@@ -0,0 +1,70 @@
+#' Left Censor Data
+#' 
+#' This function will take a data set and artificially left censor it to a request level of censoring.
+#' @param uncenData Data matrix of any size.
+#' @param cenLevelVec vector of length equal to rows of data, indicating censoring level requested in decimals.
+#' @details The function sets the limit of detection at the value which the requested amount of data is below.
+#'          All values below this limit of detection are replaced with the limit of detection.
+#' @return Matrix such that the first half of the columns are the censored data and the 
+#'         second haf are the isCensored flags with 1 indicating censoring, where censored data has a value
+#'         equal to the limit of detection.
+#' @export
+#' @examples 
+#' censorData(uncenData = myData, cenLevelVec =c(.2,.2))
+#'
+censorData <- function( uncenData, cenLevelVec){
+  # find num of col and rows in data matrix
+  numCol = length(uncenData[1,])
+  numRow = length(uncenData[,1])
+  #Declare flag matrix and fill with 0s
+  flagMat <- matrix( rep(0, times = numCol*numRow), nrow = numRow, ncol = numCol)
+  #loop thorugh data matrix to fill in flag matrix and replace "censored" values by LOD
+  for ( c in 1:numCol){
+    #Set limit of detection to catch level censoring requested
+    LoD <- quantile(uncenData[,c], cenLevelVec[c])
+    #Fill by row
+    for ( r in 1:length(uncenData[,c])){
+      if (uncenData[r,c] < LoD){
+        flagMat[r,c] <- 1
+        uncenData[r,c] <- LoD # now the uncenData is censored!!!
+      }
+    } 
+  }
+  # now the uncenData is censored!!!
+  #bind and return altered data matrix and flag together
+  cenData <- cbind(uncenData,flagMat)
+}
+
+#' Alter Censoring Method
+#' 
+#' This function will take already censored data and change 
+#' @param cenData Data matrix such that the first half of the columns are the censored data and the 
+#'         second haf are the isCensored flags with 1 indicating censoring, where censored data has a value
+#'         equal to the limit of detection.
+#' @param replacement The value request to fill censored values in the censored data matrix. 
+#'        By Defualt equals LOD divided by sqrt2 as this is a common method.
+#' @details To change the censored values to LOD/sqrt2 the data passed to the function must be censored
+#'          such that censored values = LOD. If LOD is not availible, replacement should only equal numerical values. 
+#' @return Matrix such that the first half of the columns are the censored data and the 
+#'         second haf are the isCensored flags with 1 indicating censoring, where censored data has a value
+#'         equal to replace parameter.
+#' @export
+#' @examples 
+#'
+censorDifferently <- function( cenData, replacement =LOD/sqrt(2) ){
+  # find num of col and rows in data matrix
+  numCol = length(cenData[1,])
+  numRow = length(cenData[,1])
+  # loop through and change value if flag = 1
+  for( dataCol in 1:(numCol/2)){
+    flagCol = numCol/2 + dataCol
+    for ( r in numRow){
+      if( cenData[r, flagCol] == 1){
+        LOD <- cenData[r,dataCol]
+        cenData[r,dataCol] <- replacement
+      }
+    }
+  }
+}
+
+
