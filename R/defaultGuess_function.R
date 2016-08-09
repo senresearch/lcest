9e6855c89f6cf66883bee64be0580aceb0bf74bf
Emily Hanson <hanea-17@rhodes.edu>
2016-08-09 13:28:44 -0500

    Fixed errors in documentation

--- R/defaultGuess_function.R
+++ R/defaultGuess_function.R
@@ -4,7 +4,6 @@
 #' Defualt Guess
 #' 
 #' This function picksa reasonable starting guess for optim, which is the mean, sd, and corr of the data ignoring censoring.
-#' @param thetaT transformed vector of parameters to optimize such that c(xmu, ymu, log(xsig), log(ysig), tanh(r)).
 #' @param cenData matrix of data in which first two columns hold data values for x and y and the second two
 #'        columns hold flags for censoring such that 1 implies censored and 0 implies not censored.
 #' @return guesstheta vector of length 5, guess for each xmu, ymu, xsd, ysd, rho
--- R/rescaleVarCovMat_function.R
+++ R/rescaleVarCovMat_function.R
@@ -4,7 +4,6 @@
 #' 
 #' The var cov mat is transformed when we get it from optim becuase optim works on the transformed scale. This function puts it back on the orginal or
 #' inverse of transposed scale.
-#' @param thetaT transformed vector of parameters to optimize such that c(xmu, ymu, log(xsig), log(ysig), tanh(r)).
 #' @param varCovMat a 5x5 matrix which is the inverted hessian result of optim
 #' @param transPar vector of length 5, the parameter results of optim still on transformed scale
 #' @return the varaiance covariance mat, now in orginal scale
